3495cb8a16bb1dfce8d74e60e0c82e98b5cbd048
Roel de Wit <rthdewit@gmail.com>
2014-12-02 14:08:46 +0100

    Changes to database and score calculator

--- src/Airhockey/Utils/Database.java
+++ src/Airhockey/Utils/Database.java
@@ -1,16 +1,13 @@
 package Airhockey.Utils;
 
+import Airhockey.User.User;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Calendar;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,94 +24,93 @@ import oracle.jdbc.pool.OracleDataSource;
  */
 public class Database {
 
-    private Properties props;
-    private Connection conn;
+    private Properties properties;
+    private Connection connection;
 
     public Database() {
         try {
-            InputProps();
+            inputProperties();
         } catch (IOException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void InputProps() throws FileNotFoundException, IOException {
+    private void inputProperties() throws FileNotFoundException, IOException {
         Properties props = new Properties();
         try (FileInputStream in = new FileInputStream("database.properties")) {
             props.load(in);
         }
 
-        this.props = props;
+        this.properties = props;
         configure(props);
     }
 
-    public final boolean configure(Properties props) throws IOException {
+    private final boolean configure(Properties props) throws IOException {
         try {
-            initConnection();
+            initializeConnection();
             return isCorrectlyConfigured();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
-            this.props = null;
+            this.properties = null;
             return false;
         } finally {
             closeConnection();
         }
     }
 
-    private void initConnection() throws SQLException, FileNotFoundException, IOException {
+    private void initializeConnection() throws SQLException, FileNotFoundException, IOException {
         try {
             Class.forName("oracle.jdbc.OracleDriver");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        String driver = this.props.getProperty("driver");
+        String driver = this.properties.getProperty("driver");
         if (driver != null) {
             System.setProperty("jdbc.drivers", driver);
         }
-        String url = this.props.getProperty("url");
-        String username = this.props.getProperty("username");
-        String password = this.props.getProperty("password");
+        String url = this.properties.getProperty("url");
+        String username = this.properties.getProperty("username");
+        String password = this.properties.getProperty("password");
 
         //this.conn = DriverManager.getConnection(url, username, password);
         OracleDataSource ds;
         ds = new OracleDataSource();
         ds.setURL(url);
-        this.conn = ds.getConnection(username, password);
+        this.connection = ds.getConnection(username, password);
     }
 
     private void closeConnection() {
         try {
-            if(conn != null)
-            {
-                conn.close();
+            if (connection != null) {
+                connection.close();
             }
-            conn = null;
+            connection = null;
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
         }
     }
 
-    public boolean isCorrectlyConfigured() {
-        if (props == null) {
+    private boolean isCorrectlyConfigured() {
+        if (properties == null) {
             return false;
         }
-        if (!props.containsKey("driver")) {
+        if (!properties.containsKey("driver")) {
             return false;
         }
-        if (!props.containsKey("url")) {
+        if (!properties.containsKey("url")) {
             return false;
         }
-        if (!props.containsKey("username")) {
+        if (!properties.containsKey("username")) {
             return false;
         }
-        if (!props.containsKey("password")) {
+        if (!properties.containsKey("password")) {
             return false;
         }
         return true;
     }
 
-    public boolean LoginCheckTest(String username, String password) {
+    public boolean loginCheckTest(String username, String password) {
         if (username.equals("Test") && password.equals("1234")) {
             return true;
         } else {
@@ -122,20 +118,20 @@ public class Database {
         }
     }
 
-    public boolean LoginCheck(String username, String password) throws SQLException, IOException {
+    public boolean loginCheck(String username, String password) throws SQLException, IOException {
         String usernameDB = null;
         String passwordDB = null;
 
         boolean succes = false;
 
-        initConnection();
+        initializeConnection();
 
         Statement stmt = null;
 
         String query = "SELECT USERNAME, PASSWORD FROM DBI296122.AH_USERS";
 
         try {
-            stmt = conn.createStatement();
+            stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 usernameDB = rs.getString("USERNAME");
@@ -155,4 +151,38 @@ public class Database {
         return succes;
     }
 
+    public void updateScore(User user, int score) throws SQLException, IOException {
+        int score1 = score;
+        int score2 = 15;
+        int score3 = 15;
+        int score4 = 15;
+        int score5 = 15;
+
+        Statement statement = null;
+
+        String query = "SELECT * FROM DBI296122.USERS WHERE \"username\" = '" + user.getUsername() + "'";
+
+        try {
+            initializeConnection();
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                score2 = resultSet.getInt("score1");
+                score3 = resultSet.getInt("score2");
+                score4 = resultSet.getInt("score3");
+                score5 = resultSet.getInt("score4");
+            }
+
+            double rating = ScoreCalculator.calculateRating(score1, score2, score3, score4, score5);
+
+            user.setRating(rating);
+        } catch (SQLException exception) {
+            System.out.println("SQL Exception: " + exception.getMessage());
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+        }
+    }
 }
--- src/Airhockey/Utils/ScoreCalculator.java
+++ src/Airhockey/Utils/ScoreCalculator.java
@@ -13,17 +13,17 @@ public class ScoreCalculator {
 
     /**
      * Calculate the rating of a user bases on his five most recent games
-     * @param r1 Most recent score
-     * @param r2
-     * @param r3
-     * @param r4
-     * @param r5 Earliest score
+     * @param score1 Most recent score
+     * @param score2
+     * @param score3
+     * @param score4
+     * @param score5 Earliest score
      * @return Calculated rating
      */
-    public static double calculateRating(int r1, int r2, int r3, int r4, int r5) {
+    public static double calculateRating(int score1, int score2, int score3, int score4, int score5) {
         double rating;
 
-        rating = Math.floor((5 * r1 + 4 * r4 + 3 * r3 + 2 * r2 + r1) / 15);
+        rating = Math.floor((5 * score1 + 4 * score4 + 3 * score3 + 2 * score2 + score1) / 15);
 
         return rating;
     }
