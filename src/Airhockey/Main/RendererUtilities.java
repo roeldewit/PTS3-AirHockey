7296c00730038210e88c88be46e36c528887a937
Sam Jansen <sam.jansen@student.fontys.nl>
2014-12-09 13:18:05 +0100

    Dem engine part 4.1.1, = MoreBetter

---
+++ src/Airhockey/Main/RendererUtilities.java
@@ -0,0 +1,16 @@
+package Airhockey.Main;
+
+/**
+ *
+ * @author Sam
+ */
+public class RendererUtilities {
+
+    public static int batPositionLeftToBottom() {
+        return 0;
+    }
+
+    public static int batPositionRightToBottom() {
+        return 0;
+    }
+}
