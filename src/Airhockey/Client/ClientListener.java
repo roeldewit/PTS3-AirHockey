874c494e9dd3c19e5d8e3702f25582132cb2976c
pieper126 <stijnbijnen@hotmail.com>
2014-12-10 13:49:50 +0100

    added Client and Host

---
+++ src/Airhockey/Client/ClientListener.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Airhockey.Client;
+
+/**
+ *
+ * @author pieper126
+ */
+import Airhockey.Main.Renderer;
+import Airhockey.Rmi.Location;
+import Airhockey.Rmi.RemotePropertyListener;
+import Airhockey.Rmi.RemotePublisher;
+import java.beans.PropertyChangeEvent;
+import java.io.PrintStream;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ClientListener extends UnicastRemoteObject implements RemotePropertyListener {
+
+    private RemotePublisher publisher;
+    private final Renderer renderer;
+
+    public ClientListener(Renderer renderer) throws RemoteException {
+        this.renderer = renderer;
+    }
+
+    public void connectToHost(String ipHost, int port) throws RemoteException {
+        Registry register = null;
+        this.publisher = null;
+        try {
+            register = LocateRegistry.getRegistry(ipHost, port);
+        } catch (RemoteException ex) {
+            System.err.println("Unable to find registry");
+            System.exit(-1);
+        }
+        try {
+            this.publisher = ((RemotePublisher) register.lookup("Publisher"));
+            this.publisher.addListener(this, "locations");
+        } catch (RemoteException | NotBoundException exc) {
+            System.err.println("Unable to find effectenbeurs: " + exc.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) throws RemoteException {
+        Location[] locations = (Location[]) evt.getNewValue();
+
+        this.renderer.setPuckLocation(locations[0]);
+        this.renderer.setBatLocation(locations[1]);
+        this.renderer.setLeftEnemyBat(locations[2]);
+        this.renderer.setRightEnemyBat(locations[3]);
+    }
+}
---
+++ src/Airhockey/Host/HostServer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Airhockey.Host;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.*;
+import java.rmi.registry.*;
+
+/**
+ *
+ * @author pieper126
+ */
+public class HostServer {
+
+    private static final int portNumber = 1099;
+    private static final String bindingName = "GameController";
+    private Registry registry = null;
+    private GameController gameController = null;
+
+    public HostServer() {
+        System.out.println("Server: Port number 1099");
+        try {
+            System.out.println("Server: Game controller created");
+        } catch (Exception ex) {
+            System.out.println("Server: Cannot create game controller");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+            this.gameController = null;
+        }
+        try {
+            this.registry = LocateRegistry.createRegistry(1099);
+            System.out.println("Server: Registry created on port number 1099");
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot create registry");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+            this.registry = null;
+        }
+        try {
+            this.registry.rebind("GameController", this.gameController);
+        } catch (Exception ex) {
+            System.out.println("Server: Cannot bind game controller");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+        }
+    }
+}
